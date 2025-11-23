acb3440c5f883d901f6eef7c373af7cff72916bd
Turki Ali Qaroor <145855820+Turki-Ali-Qaroor@users.noreply.github.com>
2025-11-23 17:55:10 +0300

    Add files via upload

---
+++ CV.js
@@ -0,0 +1,15 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll("header, section, footer");
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("animate");
+            } else {
+                entry.target.classList.remove("animate");
+            }
+        });
+    }, { threshold: 0.2 });
+
+    sections.forEach(section => observer.observe(section));
+});
